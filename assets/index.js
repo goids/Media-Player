d450295fd6a6b4015845c9f6975792f53c8a9fd3
Diego F. Prado <fernandoprado@iMac-de-Diego.local>
2020-08-10 20:48:48 -0500

    Closures

--- assets/index.js
+++ assets/index.js
@@ -9,11 +9,68 @@ button.onclick = () => player.activeVideo();
 
 /**
  * IIFE
- * Inmmediately-invoked function expressions
+ * Inmmediately-invoked function expressions, expresion funcional que se ejecutra inmediatamente
  * (function(){
  *    .... codigo
  * })();
+ * CLOSURES o CLAUSURAS
+ * Es una funcion interna que tiene acceso al alcance  de su funcion externa
+ * gracias a los Closures podemos crear variables internas
  */
+
+
+/*(function(){
+    let color = 'red';
+
+    function printColor(){
+        console.log(color);
+    }
+
+    printColor();
+ })();
+
+ function makeColorPrinter(color){
+    let colorMEssage = `The color is ${color}`;
+    return function(){
+        console.log(colorMEssage);
+    }
+ }
+
+ let redColorPrinter = makeColorPrinter('red'); //Esta funcion se aceurda del valor de color, aunque la estemos llamndo desde afuera
+ console.log(redColorPrinter()); //por eso obtenemos el resultado aquí
+
+ function makeCounter(n){
+    let count = n; //La variable count, logramos que fuera privada
+
+    return {
+        increase: function(){
+            count += 1;
+        },
+        decrease: function(){
+            count -= 1;
+        },
+        getCount: function(){
+            return count;
+        }
+    }
+ }
+
+ let counter = makeCounter(5);
+ console.log('The count is: ', counter.getCount());
+ counter.increase();
+ counter.increase();
+ console.log('The count is: ', counter.getCount());
+ counter.increase();
+ console.log('The count is: ', counter.getCount());
+ counter.decrease();
+ counter.decrease();
+ counter.decrease();
+ console.log('The count is: ', counter.getCount());*/
+
+ // Cuando se retorna una funcion o algún objeto o variable, debemos llamarla con los parentesis
+ // En este caso, console.log(redColorPrinter());
+ // Si la funcion no me retornara nada la llamamos de la siguiente manera
+ // console.log(redColorPrinter);
 
 //CLASE ESCRITA CON CLAS CLASES EN JS DE ES6
       
