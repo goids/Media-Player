86a56d55a9ea9e95904974c8d53c16255a47f870
Diego F. Prado <fernandoprado@iMac-de-Diego.local>
2020-08-12 10:04:51 -0500

    Se agrego el primer plugin, el plugin de autoplay

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -1,6 +1,15 @@
 //Crearemos una clase en JS, como sabemos son prototipos
 function MediaPlayer(config){
-    this.media = config.el
+    this.media = config.el;
+    this.plugins = config.plugins || [];
+    
+    this._initPlugins();
+}
+
+MediaPlayer.prototype._initPlugins = function(){
+    this.plugins.forEach(plugin => {
+        plugin.run(this);
+    });
 }
 
 MediaPlayer.prototype.play = function(){
@@ -13,12 +22,28 @@ MediaPlayer.prototype.pause = function(){
 
 MediaPlayer.prototype.activeVideo = function(){
     if(this.media.paused){
-        this.media.play();
+        this.play();
     }else if(this.media.played){
-        this.media.pause();
+        this.pause();
     }
     //Ternario 
-    //(this.media.paused) ? this.media.play() : this.media.pause();
+    //(this.media.paused) ? this.play() : this.pause();
+}
+
+MediaPlayer.prototype.mute = function(){
+    this.media.muted = true;
+}
+
+MediaPlayer.prototype.unmute = function(){
+    this.media.muted = false;
+}
+
+MediaPlayer.prototype.activeSound = function(){
+    if(this.media.muted){
+        this.unmute();
+    }else{
+        this.mute();
+    }
 }
 
 export default MediaPlayer;
