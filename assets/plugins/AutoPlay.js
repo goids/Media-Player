682fff91db1f04d7f6ac8dc8b66506dc53c9bcf6
Diego F. Prado <fernandoprado@iMac-de-Diego.local>
2020-08-13 20:18:35 -0500

    Llamada del get y el set desde el plugin autoplay

--- assets/plugins/AutoPlay.js
+++ assets/plugins/AutoPlay.js
@@ -1,8 +1,12 @@
 function AutoPlay(){}
 
 AutoPlay.prototype.run = function(player){
-    player.mute();
+    //player.mute(); // ya no podemos aplicar el player.muted() directamente
+    if (!player.muted) { //Los set lo llamamos asignandole un valor
+        player.muted = true; //como es un setter de nuestra propiedades virtuales, vamos a pasarle true de esta manera, ya que a los set se les pasa parametros de esta manera
+    }
     player.play();
+    player.self;
 }
 
 export default AutoPlay;
