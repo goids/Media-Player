5257af4d1aaefe02431462485a05bc126f7b6334
Diego F. Prado <fernandoprado@iMac-de-Diego.local>
2020-08-20 12:21:48 -0500

    Implementado la API del DOM, IntersectionObserver y visibilityChange

--- assets/plugins/AutoPause.js
+++ assets/plugins/AutoPause.js
@@ -2,15 +2,19 @@ class AutoPause{
     constructor(){
         this.threshold = 0.25;
         this.handleIntersection = this.handleIntersection.bind(this);
+        this.handleVisibilityChange = this.handleVisibilityChange.bind(this);
     }
 
     run(player){
         this.player = player;
         const observer = new IntersectionObserver(this.handleIntersection, { // aqui se pasa mediante el bind(this), por que el this, hace referencia  
-            threshold : this.threshold                                       // a la funcion del objeto, la cual es IntersectionObserver
+            threshold : this.threshold                                       // al objeto que llama la funcion, la cual es IntersectionObserver
         });
 
-        observer.observe(player.media)
+        observer.observe(player.media);
+
+        // visibilitychange
+        document.addEventListener('visibilitychange', this.handleVisibilityChange);
     }
 
     handleIntersection(entries){
@@ -22,9 +26,31 @@ class AutoPause{
             this.player.play();
         }else{
             this.player.pause();
+        }          
+    }
+
+    handleVisibilityChange(){
+        //this dentro de esta funcion sin el bind, no es la clase, es: #document
+        let isVisible = document.visibilityState === 'visible';
+        this.toggleVideo(isVisible);
+    }
+
+    /**
+     * TIP
+     * Hice la prueba con las arrow functions para evitar pasar el this por el metodo bind
+     * this, dentro de la funcion arrow, termino siendo mi clase AutoPause
+     * 
+     * esta funcion es equivalente a la que tengo arriba que le paso el this por bind,
+     * 
+     * handleVisibilityChange = () =>{
+            let isVisible = document.visibilityState === 'visible';
+            console.log(this); //clase AutoPause
+            this.toggleVideo(isVisible);
         }
-        
-        
+     */
+
+    toggleVideo(bool){
+        (bool) ? this.player.play() : this.player.pause(); 
     }
 }
 
